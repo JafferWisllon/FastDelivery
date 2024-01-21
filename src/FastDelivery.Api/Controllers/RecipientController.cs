49b030b3557a13f75f3f3501130cb0e70f76b8e9
Jaffer Wisllon <contatojaffer@gmail.com>
2024-01-21 11:56:40 -0300

    [FEATURE] mapping all recipients endpoints and include open api analyzer

--- src/FastDelivery.Api/Controllers/RecipientController.cs
+++ src/FastDelivery.Api/Controllers/RecipientController.cs
@@ -4,25 +4,47 @@ using Microsoft.AspNetCore.Mvc;
 namespace FastDelivery.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/recipient")]
     public class RecipientController : ControllerBase
     {
-        [HttpGet]
-        public Recipient Get()
+        [HttpGet()]
+        [ProducesResponseType(typeof(Recipient), StatusCodes.Status200OK)]
+        public IActionResult Get()
         {
-            var recipient = new Recipient()
-            {
-                Id = 1,
-                Name = "Home",
-                Street = "",
-                City = "",
-                Complement = "",
-                Number = "",
-                State = "state",
-                ZipCode = ""
-            };
+            return Ok(new Recipient() { Id = 1, Name = "Home" });
+        }
 
-            return recipient;
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Recipient), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Recipient() { Id = id });
+        }
+
+        [HttpPost()]
+        [ProducesResponseType(typeof(Recipient), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create([FromBody] Recipient recipient)
+        {
+            return Ok(new Recipient() { Id = recipient.Id });
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update(int id, [FromBody] Recipient recipient)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
         }
     }
 }
