64f4cb3efe1cf1a3eacfceff66b564f899f193b1
Jaffer Wisllon <contatojaffer@gmail.com>
2024-01-21 11:14:07 -0300

    [FEATURE] add Recipient controller

---
+++ src/FastDelivery.Api/Controllers/RecipientController.cs
@@ -0,0 +1,28 @@
+﻿using FastDelivery.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastDelivery.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipientController : ControllerBase
+    {
+        [HttpGet]
+        public Recipient Get()
+        {
+            var recipient = new Recipient()
+            {
+                Id = 1,
+                Name = "Home",
+                Street = "",
+                City = "",
+                Complement = "",
+                Number = "",
+                State = "state",
+                ZipCode = ""
+            };
+
+            return recipient;
+        }
+    }
+}
---
+++ src/FastDelivery.Api/Models/Recipient.cs
@@ -0,0 +1,14 @@
+﻿namespace FastDelivery.Api.Models
+{
+    public class Recipient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
