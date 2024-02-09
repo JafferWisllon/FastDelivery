9d094e5c54e1a087585a1311cb28192d74b3bbdd
Jaffer Wisllon <contatojaffer@gmail.com>
2024-02-08 21:30:02 -0300

    [FEATURE] add deliveryman management

--- src/FastDelivery.Api/Migrations/20240208235800_add-deliveryman.Designer.cs
+++ src/FastDelivery.Api/Migrations/20240208235800_add-deliveryman.Designer.cs
@@ -4,6 +4,7 @@ using FastDelivery.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FastDelivery.Api.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240208235800_add-deliveryman")]
+    partial class adddeliveryman
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FastDelivery.Api/ViewModels/DeliverymanViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastDelivery.Api.ViewModels
+{
+    public class DeliverymanViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string AvatarId { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
